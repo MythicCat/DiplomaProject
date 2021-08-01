4e67fc98a0e19f8c84a3775db35e8136f15a9e6e
MadWorg <45514553+MadWorg@users.noreply.github.com>
2021-08-01 21:27:41 +0200

    Added cannonball explosion effect

--- Hazards/Cannonball.gd
+++ Hazards/Cannonball.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
-const SPEED = 200
+export var speed = 200
+
+onready var ball = $Area2D/Sprite
+onready var explosion = $AnimatedSprite
 
 func _ready():
 	set_as_toplevel(true)
 	global_position = get_parent().global_position
 
 func _process(delta):
-	position.x -= SPEED * delta
+	position.x -= speed * delta
 	detect_collision()
 
 func _on_VisibilityNotifier2D_screen_exited():
@@ -18,4 +21,13 @@ func detect_collision():
 	for object in collider:
 		if object.name == "Player":
 			get_tree().call_group("GameState", "hurt", position)
+		ball.visible = false
+		speed = 0
+		explosion.visible = true
+		explosion.play("explode")
+		
+
+
+func _on_AnimatedSprite_animation_finished():
+	if explosion.animation == "explode":
 		queue_free()
